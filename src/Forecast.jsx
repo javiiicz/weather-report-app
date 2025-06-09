464e8c1e10c568282df07e41759e907776146244
Javier Carrillo Zamora <javiercz@meta.com>
2025-06-09 12:43:16 -0700

    Frontend: Add Forecast component

---
+++ src/Forecast.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+const Forecast = () => {
+    return (
+        <div className="forecast-container">
+            Weather Forecast woll go here
+        </div>
+    )
+}
+
+export default Forecast;

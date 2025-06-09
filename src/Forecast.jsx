b3f6592ba1bf174448e333763ecde48dc45bd1aa
Javier Carrillo Zamora <javiercz@meta.com>
2025-06-09 12:58:19 -0700

    Frontend : refactor style files

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Day from "./Day";
+import "./Forecast.css"
+
 
 const Forecast = () => {
     return (
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./SearchForm.css"
 
 const SearchForm = () => {
     const submitSearch = (event) => {

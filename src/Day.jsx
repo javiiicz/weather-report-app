7ed70b5fbcdad2bc99cdab1cb9427533d5586d58
Javier Carrillo Zamora <javiercz@meta.com>
2025-06-09 12:56:20 -0700

    Frontend: Add day component

---
+++ src/Day.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import PropTypes from "prop-types";
+
+const Day = (props) => {
+    return (
+        <div className="day-container">
+            <h2>{props.date}</h2>
+            <p>{props.weather}</p>
+            <p>{props.temperature}°C</p>
+        </div>
+    )
+}
+
+Day.propTypes = {
+    date: PropTypes.string.isRequired,
+    weather: PropTypes.string.isRequired,
+    temperature: PropTypes.number.isRequired
+}
+
+export default Day;
--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import Day from "./Day";
 
 const Forecast = () => {
     return (
         <div className="forecast-container">
-            Weather Forecast will go here
+            <Day date="Thu, Mar 14"  weather="Cloudy" temperature={21.0}/>
+            <Day date="Fri, Mar 15"  weather="Sunny" temperature={24.4}/>
+            <Day date="Sat, Mar 16"  weather="Rain" temperature={19.0}/>
+            <Day date="Sun, Mar 17"  weather="Thunder" temperature={18.0}/>
+            <Day date="Mon, Mar 18"  weather="Hazy" temperature={15.0}/>
         </div>
     )
 }
